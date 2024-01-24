df6fb5f76547db273da787531310077f46142c40
impishian <impishian@gmail.com>
2024-01-24 17:59:36 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Depending on your compilation environment, "-lpthread" may not be needed.
+# Try the -Os, -O2, -O3, -Ofast compilation options.
+
 gcc -I. -o facil *.c -O2 -march=native  -DNDEBUG -lpthread
 
 strip facil
