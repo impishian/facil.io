161638d3b88603a48cf2d3839262336b24be0e6d
impishian <impishian@gmail.com>
2024-01-29 15:14:56 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,4 +5,10 @@
 
 gcc -I. -o facil *.c -O2 -march=native  -DNDEBUG -lpthread
 
+# glib, static:
+# gcc -I.  -Os -static -march=native -static-libgcc *.c -Os -o facil
+
+# musl:
+# musl-gcc -I. -static  -Os *.c -o facil
+
 strip facil
